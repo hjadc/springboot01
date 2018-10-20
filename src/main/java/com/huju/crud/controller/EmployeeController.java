218bc931b31982c2d8c6ff00f0a2a1183bb984e9
huju <13886338732@163.com>
2018-10-20 23:22:51 +0800

    crud员工添加页面

--- src/main/java/com/huju/crud/controller/EmployeeController.java
+++ src/main/java/com/huju/crud/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.huju.crud.controller;
 
+import com.huju.crud.dao.DepartmentDao;
 import com.huju.crud.dao.EmployeeDao;
+import com.huju.crud.entities.Department;
 import com.huju.crud.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,7 +17,9 @@ import java.util.Collection;
 @Controller
 public class EmployeeController {
     @Autowired
-    EmployeeDao employeeDao;
+    EmployeeDao employeeDao;     // 获取员工
+    @Autowired
+    DepartmentDao departmentDao; // 获取部门
 
     /**
      * 查询全部的员工
@@ -29,5 +33,19 @@ public class EmployeeController {
         model.addAttribute("emps", all);
 
         return "emp/list";
+    }
+
+    /**
+     * 去添加员工页面
+     * @param model
+     * @return
+     */
+    @GetMapping("/emp")
+    public String toAddPage(Model model) {
+        // 获取部门集合,放在请求域中,给页面做下拉框用
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+
+        return "emp/add";
     }
 }
